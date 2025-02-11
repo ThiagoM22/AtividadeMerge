49a3dc87d4620f26eb3eaf85189c85dcc61a6b42
Brun0 <brunodev41@gmail.com>
2025-02-11 16:03:59 -0300

    Classe Brun0

---
+++ AtividadeMerge/Brun0.cs
@@ -0,0 +1,9 @@
+﻿namespace AtividadeMerge
+{
+    public class Brun0
+    {
+        public int Peso { get; set; }
+        public int Altura { get; set; }
+        public int Idade { get; set; }
+    }
+}
