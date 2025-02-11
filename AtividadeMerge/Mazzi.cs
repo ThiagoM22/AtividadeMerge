9bb7acc08885476d4a4cec14c0cb33f46f1814e8
Brun0 <brunodev41@gmail.com>
2025-02-11 16:16:41 -0300

    Alterando classe mazzi

--- AtividadeMerge/Mazzi.cs
+++ AtividadeMerge/Mazzi.cs
@@ -4,6 +4,8 @@
     {
         public int MazziId { get; set; }
         public string Nome { get; set; }
-        public string Sobrenome { get; set; }
+        public string UltimoNome { get; set; }
+
+        public int SobrenomeId { get; set; }
     }
 }
